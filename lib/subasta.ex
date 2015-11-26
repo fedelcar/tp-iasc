642aadda84107dfe4faaa796b2d1a0ad30e5ed2d
Sebastian Balay <snbalay@gmail.com>
2015-11-26 00:49:17 -0300

    listo los test del escenario 1

--- lib/subasta.ex
+++ lib/subasta.ex
@@ -11,6 +11,10 @@ defmodule Subasta do
     GenServer.cast(server, {:create, {key, values}})
   end
 
+  def ofertar(server, name, price, offerer) do
+    GenServer.call(server, {:ofertar, {name, price, offerer}})
+  end
+
   ## Server Callbacks
 
   def start_link(ets, opts \\ []) do
@@ -23,6 +27,13 @@ defmodule Subasta do
         {:noreply, ets}
       :not_found ->
         :ets.insert(ets, {key, values})
+        case key do
+          {_, :subasta} ->
+            IO.puts "todos putos"
+            IO.puts length :ets.match(ets, '_')
+          _ ->
+        end
+
         {:noreply, ets}
     end
   end
@@ -36,10 +47,33 @@ defmodule Subasta do
     end
   end
 
+  def handle_call({:ofertar, {name, price, offerer}}, _from, ets) do
+      case lookup_ets(ets, {name, :subasta}) do
+      {:ok, {{name, _}, {winning_price, _, _}}} ->
+        if price <= winning_price do
+          {:reply, {:bad_request, "Oferta mas baja a la ganador gato"}, ets}
+        else
+          ## puts price in the subasta
+          update_price(ets, name, price, offerer)
+          IO.puts 'tamo'
+          {:reply, {:ok, "Todo piola"}, ets}
+        end
+      :not_found ->
+        {:reply, :not_found, ets}
+    end
+  end
+
   def lookup_ets(ets, key) do
     case :ets.lookup(ets, key) do
       [entity] -> {:ok, entity}
       [] -> :not_found
     end
+  end
+
+  def update_price(ets, name, price, offerer) do
+    key = {name, :subasta}
+    {:ok, {_, {_, duration, _}}} = lookup_ets(ets, key)
+    :ets.delete(ets, key)
+    :ets.insert(ets, {key, {price, duration, offerer}})
   end
 end
---
+++ test/escenario1and2_test.exs
@@ -0,0 +1,26 @@
+defmodule Escenario1and2_Test do
+  use ExUnit.Case
+
+  setup do
+    ets = :ets.new(:registry_table, [:set, :public])
+    {:ok, subasta} = Subasta.start_link(ets, [])
+    {:ok, subasta: subasta}
+  end
+
+  test "escenario1and2", %{subasta: subasta} do
+    miguel = Subasta.create(subasta, {{"miguel", :comprador}, {"miguel@miguel.miguel"}})
+    jorge = Subasta.create(subasta, {{"jorge", :comprador}, {"jorge@jorge.jorge"}})
+    anibal = Subasta.create(subasta, {{"anibal", :comprador}, {"anibal@anibal.anibal"}})
+
+    Subasta.create(subasta, {{"vendo auto", :subasta}, {10, 3, nil}}) # missing notification to al buyers
+
+    Subasta.ofertar(subasta, "vendo auto", 15, jorge)
+    Subasta.ofertar(subasta, "vendo auto", 12, anibal)
+    Subasta.ofertar(subasta, "vendo auto", 17, anibal)
+
+    :timer.sleep(3000)
+    # assert that bidding finished
+    assert {:ok, {{"vendo auto", :subasta}, {17, 3, anibal}}} = Subasta.lookup(subasta, {"vendo auto", :subasta})
+    # assert notification to winner and losers
+  end
+end
