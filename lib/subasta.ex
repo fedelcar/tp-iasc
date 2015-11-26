d519c661668aae132ce18e9232a68aa47bf1dbd7
mpetrone <matias.petrone@gmail.com>
2015-11-25 21:19:32 -0300

    agrego el lookup y el create en subastas

--- lib/subasta.ex
+++ lib/subasta.ex
@@ -1,15 +1,46 @@
 defmodule Subasta do
   use GenServer
 
-  def start_link(state, opts \\ []) do
-    GenServer.start_link(__MODULE__, state, opts)
+  ## Client API
+
+  def lookup(server, name) do
+    GenServer.call(server, {:lookup, name})
   end
 
-  def handle_cast({:create, name}, state) do
-    case :ets.lookup(state, name) do
-      [{^name, subasta}] -> {:noreply, state}
-      [] -> {:noreply, state}
-    end 
+  def create(server, name) do
+    GenServer.cast(server, {:create, name})
   end
 
+
+  ## Server Callbacks
+
+  def start_link(ets, opts \\ []) do
+    GenServer.start_link(__MODULE__, ets, opts)
+  end
+
+  def handle_cast({:create, name}, ets) do
+    case lookup(ets, name) do
+      {:ok, subasta} ->
+        {:noreply, ets}
+      :not_found ->
+        :ets.insert(ets, {name})
+        {:noreply, ets}
+    end
+  end
+
+  def handle_call({:lookup, name}, _from, ets) do
+    case lookup(ets, name) do
+      {:ok, subasta} ->
+        {:reply, {:ok, subasta}, ets}
+      :not_found ->
+        {:reply, :not_found, ets}
+    end
+  end
+
+  def lookup(ets, name) do
+    case :ets.lookup(ets, name) do
+      [{^name, subasta}] -> {:ok, subasta}
+      [] -> :not_found
+    end
+  end
 end
