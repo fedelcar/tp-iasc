255170a4078a0301b2221719d2746eb6fdf2027b
fedelcar <federico.lopez@wolox.com.ar>
2015-11-27 15:24:59 -0300

    Escenario 2

--- test/escenario2_test.exs
+++ test/escenario2_test.exs
@@ -18,33 +18,44 @@ defmodule Escenario2Test do
     {:ok, plataforma: plataforma}
   end
 
-  
+
 
 # Similar al escenario anterior, pero antes de terminar la subasta, B oferta un precio mayor,
 # y al cumplirse el plazo, se le adjudica a éste.
 # Obviamente, este proceso de superar la oferta anterior puede repetirse indefinidamente mientras la subasta esté abierta.
 
-  test "escenario2", %{plataforma: plataforma} do
+  test "Escenario 2", %{plataforma: plataforma} do
+
+    # Se registran los dos compradores
     Plataforma.create_comprador(plataforma, "arya stark", "deathismyfried@gmail.com")
     Plataforma.create_comprador(plataforma, "john snow", "idontknownothing@gmail.com")
 
+    # Nueva subasta y notifiación a todos de la misma
     Plataforma.create_subasta(plataforma, "se vende heladera", 10, 1)
     assert_receive {:new_subasta, "arya stark", "se vende heladera"}
     assert_receive {:new_subasta, "john snow", "se vende heladera"}
 
+    # Nueva oferta y notificación a todos de la misma
     Plataforma.ofertar(plataforma, "se vende heladera", 15, "john snow")
     assert_receive {:oferta_aceptada, "john snow", "se vende heladera", 15}
     assert_receive {:oferta, "arya stark", "se vende heladera", 15, "john snow"}
 
+     # Nueva oferta, pero es inferior a la ganadora, por lo que se ignora
+    Plataforma.ofertar(plataforma, "se vende heladera", 10, "arya stark")
+
+    # Nueva oferta superior y notificación a todos de la misma
     Plataforma.ofertar(plataforma, "se vende heladera", 200, "arya stark")
     assert_receive {:oferta_aceptada, "arya stark", "se vende heladera", 200}
     assert_receive {:oferta, "john snow", "se vende heladera", 200, "arya stark"}
 
-    # Notificacion a los clientes de la oferta
-
+    # Esperamos a que termine la subasta
     :timer.sleep(1000)
-    # Notificacion a los clientes de la subasta finalizada
 
+    # Notificacion a los clientes de la subasta finalizada
+    assert_receive {:subasta_finished, "john snow", "se vende heladera"}
+    assert_receive {:subasta_finished, "arya stark", "se vende heladera"}
+
+    # Corroboramos quién ganó la subasta
     {:ok, subasta} = Plataforma.lookup_subasta(plataforma, "se vende heladera")
     assert subasta.name == "se vende heladera"
     assert subasta.price == 200
